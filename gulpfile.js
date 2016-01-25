b6e7f17af7771c23ef1d50309ae58e89503bee23
Jacob <jacob@wikiver.se>
2016-01-25 10:56:27 +0100

    added babelify es6 code

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-//var rename = require('gulp-rename');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
 // var gutil = require('gulp-util');
 // var gulpif = require('gulp-if');
 // var streamify = require('gulp-streamify');
@@ -11,9 +18,6 @@ var uglify = require('gulp-uglify');
 // var less = require('gulp-less');
 // var plumber = require('gulp-plumber');
 // var source = require('vinyl-source-stream');
-// var babelify = require('babelify');
-// var browserify = require('browserify');
-// var watchify = require('watchify');
 
 //3rd party scripts minification
 gulp.task('vendor', function() {
@@ -24,6 +28,82 @@ gulp.task('vendor', function() {
     .pipe(uglify())
     .pipe(gulp.dest('public/javascripts'));
 });
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+
+
+ // function bundle_js(bundler) {
+ //   return bundler.bundle()
+ //     //.on('error', map_error)
+ //     .pipe(source('wikiverse.js'))
+ //     .pipe(buffer())
+ //     .pipe(gulp.dest('public/javascripts'))
+ //     .pipe(rename('wikiverse.min.js'))
+ //     .pipe(sourcemaps.init({ loadMaps: true }))
+ //       // capture sourcemaps from transforms
+ //     .pipe(uglify())
+ //     .pipe(sourcemaps.write('.'))
+ //     .pipe(gulp.dest('public/javascripts'))
+ // }
+
+ // // Without watchify
+ // gulp.task('browserify', function () {
+ //   var bundler = browserify('public/javascripts/es6/wikiverse.js', { debug: true }).transform(babelify, { presets: ['es2015'] })
+
+ //   return bundle_js(bundler)
+ // })
+ // 
+ // Without watchify
+ gulp.task('browserify', function () {
+   return browserify('public/javascripts/es6/wikiverse.js', { debug: true })
+   	.transform(babelify, { presets: ['es2015'] })
+   	.bundle()
+   	.pipe(source('_wikiverse.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('public/javascripts'));
+ })
+
+// gulp.task('browserify', function() {
+//   return browserify('public/javascripts/es6/wikiverse.js')
+//     //.external(dependencies)
+//     .transform("babelify", { presets: ['es2015'] })
+//     .bundle()
+//     // .pipe(source('bundle.js'))
+//     // .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+//     //.pipe(uglify())
+//     //.pipe(rename({suffix: '.min'}))
+//     .pipe(gulp.dest('public/javascripts'));
+// });
+
+/*
+ |--------------------------------------------------------------------------
+ | Same as browserify task, but will also watch for changes and re-compile.
+ |--------------------------------------------------------------------------
+ */
+// gulp.task('browserify-watch', ['browserify-vendor'], function() {
+//   var bundler = watchify(browserify('app/main.js', watchify.args));
+//   bundler.external(dependencies);
+//   bundler.transform(babelify, { presets: ['es2015', 'react'] })
+//   bundler.on('update', rebundle);
+//   return rebundle();
+
+//   function rebundle() {
+//     var start = Date.now();
+//     return bundler.bundle()
+//       .on('error', function(err) {
+//         gutil.log(gutil.colors.red(err.toString()));
+//       })
+//       .on('end', function() {
+//         gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+//       })
+//       .pipe(source('bundle.js'))
+//       .pipe(gulp.dest('public/js/'));
+//   }
+// });
 
 
 gulp.task('default', function() {
