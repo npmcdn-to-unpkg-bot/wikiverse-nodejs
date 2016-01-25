7105eaa3a0ce81d5beed157b9de415bba32c56b8
Jacob <jacob@wikiver.se>
2016-01-25 08:19:45 +0100

    built board jade rendering route

--- controllers/boardRender.js
+++ controllers/boardRender.js
@@ -2,7 +2,7 @@
 var Board = require('../models/board');
 
 // Create endpoint /api/boards/:board_id for GET
-exports.getBoardTemplate = function(req, res) {
+exports.getBoardRender = function(req, res) {
   // Use the Board model to find a specific board
   Board.findById(req.params.board_id, function(err, board) {
     if (err)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var boardRenderController = require('../controllers/boardRender');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Create endpoint handlers for /boards
+// router.route('/boards')
+//   .post(boardController.postBoards)
+//   .get(boardController.getBoards);
+
+// Create endpoint handlers for /boards/:board_id
+router.route('/boards/:board_id')
+  .get(boardRenderController.getBoardRender);
 
 module.exports = router;
